8abbe395c0ddd7cc9ebde3612461aad260f3e367
adatena <49405626+adatena@users.noreply.github.com>
2020-10-20 21:28:34 +0200

    cleaned script to save all data to csv

--- WiseairDataToCsv.py
+++ WiseairDataToCsv.py
@@ -1,16 +1,13 @@
 from lib.WiseairClient import WiseairClient
 from lib.WiseairClient import WiseairUtils
 import pandas as pd
-import time
 
 client = WiseairClient(r"./lib/personalAccessToken.csv")
 
 currentMeasures = client.getLiveAirQuality()
-#print(currentMeasures)
 
 potIdList = []
 
-# cycle over received informations
 for item in currentMeasures["data"]:
     potIdList.append(item["pot_id"])
 
@@ -18,10 +15,9 @@ print(potIdList)
 print(len(potIdList))
 
 utils = WiseairUtils()
-# get data for each pot
 
 wiseairdata=pd.DataFrame()
-count = 0
+
 potIdList.remove(1040)
 potIdList.remove(1036)
 potIdList.remove(1025)
@@ -34,12 +30,5 @@ for ID in potIdList:
     data = client.getDataOfPotByInterval(ID,BEGIN_DATE,END_DATE)
     data=utils.getPandasDataFrameFromDataOfSingleSensor(data)
     wiseairdata= pd.concat([wiseairdata,data])
-    count += 1
-    #if count == 10:
-        #time.sleep(40)
-        #count=0
-        
-
-wiseairdata.info()
 
 wiseairdata.to_csv("wiseairdata.csv")
